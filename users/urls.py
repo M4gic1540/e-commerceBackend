8b38c2589e35afc9ab52f129ec948263e8042265
tomas <t.gonzalezb@duocuc.cl>
2025-01-28 10:56:14 -0300

    The URLs are updated and the logout URL is added.

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, LogoutView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='usuarios')
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ] + router.urls
